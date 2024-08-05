3935d17667f3693cc45aa7790814f09c42e1ae12
Kanad Shee <codewizardkanad@gmail.com>
2024-08-06 01:14:58 +0530

    modified shimmer

--- Spotify/src/Shimmer/Shimmer.jsx
+++ Spotify/src/Shimmer/Shimmer.jsx
@@ -4,21 +4,29 @@ import "./Shimmer.css";
 const Shimmer = () => {
   return (
     <div className="shimmer-wrapper">
-      <div className="shimmer-title shimmer"></div>
-      <div className="shimmer-card">
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
+      <div className="shimmer-sidebar">
         <div className="shimmer"></div>
         <div className="shimmer"></div>
       </div>
-      <div className="shimmer-title shimmer"></div>
-      <div className="shimmer-card">
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
-        <div className="shimmer"></div>
+      <div className="shimmer-content">
+        <div className="shimmer-heading shimmer"></div>
+        <div className="shimmer-card-container">
+          <div className="shimmer-card shimmer"></div>
+          <div className="shimmer-card shimmer"></div>
+          <div className="shimmer-card shimmer"></div>
+          <div className="shimmer-card shimmer"></div>
+        </div>
+        <div className="shimmer-heading shimmer"></div>
+        <div className="shimmer-card-container">
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+          <div className="shimmer-card small shimmer"></div>
+        </div>
       </div>
     </div>
   );
