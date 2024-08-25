79466d83536300538ec18efc6bfee8a08ede71d7
Kanad Shee <codewizardkanad@gmail.com>
2024-08-25 22:58:31 +0530

    Added slider for albums

--- Spotify/src/components/DisplayHome.jsx
+++ Spotify/src/components/DisplayHome.jsx
@@ -4,43 +4,93 @@ import AlbumItem from "./AlbumItem";
 import SongItem from "./SongItem";
 import { useContext } from "react";
 import { PlayerContext } from "../context/PlayerContext";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const DisplayHome = () => {
-  const { songsData, albumsData } = useContext(PlayerContext);
+    const { songsData, albumsData } = useContext(PlayerContext);
 
-  return (
-    <>
-      <Navbar />
-      <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-        <div className="flex overflow-auto">
-          {albumsData.map((item, index) => (
-            <AlbumItem
-              key={index}
-              name={item.name}
-              desc={item.desc}
-              id={item._id}
-              image={item.image}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Today's Biggest Hits</h1>
-        <div className="flex overflow-auto">
-          {songsData.map((item, index) => (
-            <SongItem
-              key={index}
-              name={item.name}
-              desc={item.desc}
-              id={item._id}
-              image={item.image}
-            />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+    const slideLeft = () => {
+        const slider = document.getElementById("slider");
+        slider.scrollLeft -= 250;
+    };
+    const slideRight = () => {
+        const slider = document.getElementById("slider");
+        slider.scrollLeft += 250;
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className="mb-4">
+                <h1 className="my-5 text-2xl font-bold">Featured Charts</h1>
+                <div className="relative">
+                    <div className="absolute flex items-center gap-2 justify-end -top-14 right-0 z-[5]">
+                        <button
+                            onClick={slideLeft}
+                            className="p-2 rounded-full bg-slate-800 text-slate-500 hover:text-slate-300 hover:bg-slate-600"
+                        >
+                            <FaArrowLeft />
+                        </button>
+                        <button
+                            onClick={slideRight}
+                            className="p-2 rounded-full bg-slate-800 text-slate-500 hover:text-slate-300 hover:bg-slate-600"
+                        >
+                            <FaArrowRight />
+                        </button>
+                    </div>
+                    <div
+                        id="slider"
+                        className="flex w-full h-full overflow-x-scroll scroll-smooth scrollbar-hide"
+                    >
+                        {albumsData.map((item, index) => (
+                            <AlbumItem
+                                key={index}
+                                name={item.name}
+                                desc={item.desc}
+                                id={item._id}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="mb-4">
+                <h1 className="my-5 text-2xl font-bold">
+                    Today's Biggest Hits
+                </h1>
+                <div className="relative">
+                    <div className="absolute flex items-center gap-2 justify-end -top-14 right-0 z-[5]">
+                        <button
+                            onClick={slideLeft}
+                            className="p-2 rounded-full bg-slate-800 text-slate-500 hover:text-slate-300 hover:bg-slate-600"
+                        >
+                            <FaArrowLeft />
+                        </button>
+                        <button
+                            onClick={slideRight}
+                            className="p-2 rounded-full bg-slate-800 text-slate-500 hover:text-slate-300 hover:bg-slate-600"
+                        >
+                            <FaArrowRight />
+                        </button>
+                    </div>
+                    <div
+                        id="slider"
+                        className="flex w-full h-full overflow-x-scroll scroll-smooth scrollbar-hide"
+                    >
+                        {songsData.map((item, index) => (
+                            <SongItem
+                                key={index}
+                                name={item.name}
+                                desc={item.desc}
+                                id={item._id}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default DisplayHome;
--- Spotify/src/components/Navbar.jsx
+++ Spotify/src/components/Navbar.jsx
@@ -3,50 +3,52 @@ import { assets } from "../assets/frontend-assets/assets";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const url = "https://spotify-backend-r71h.onrender.com";
+    const url = "https://spotify-backend-r71h.onrender.com";
 
-  return (
-    <>
-      <div className="w-full flex justify-between items-center font-semibold">
-        <div className="flex items-center gap-2">
-          <img
-            onClick={() => navigate(-1)}
-            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
-            src={assets.arrow_left}
-            alt=""
-          />
-          <img
-            onClick={() => navigate(1)}
-            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
-            src={assets.arrow_right}
-            alt=""
-          />
-        </div>
-        <div className="flex items-center gap-4">
-          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer hover:bg-black hover:text-white">
-            Explore Premium
-          </p>
-          <p className="bg-black cursor-pointer py-1 px-3 rounded-2xl text-[15px] hover:bg-white hover:text-black">
-            Install App
-          </p>
-          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
-            K
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center gap-2 mt-4">
-        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
-          All
-        </p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
-          Podcasts
-        </p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex items-center justify-between w-full font-semibold">
+                <div className="flex items-center gap-2">
+                    <img
+                        onClick={() => navigate(-1)}
+                        className="w-8 p-2 bg-black cursor-pointer rounded-2xl"
+                        src={assets.arrow_left}
+                        alt=""
+                    />
+                    <img
+                        onClick={() => navigate(1)}
+                        className="w-8 p-2 bg-black cursor-pointer rounded-2xl"
+                        src={assets.arrow_right}
+                        alt=""
+                    />
+                </div>
+                <div className="flex items-center gap-4">
+                    <p className="bg-slate-700 bg-opacity-45 text-slate-200 text-[15px] px-4 py-2 rounded-lg hidden md:block cursor-pointer hover:bg-opacity-75 hover:text-white">
+                        Explore Premium
+                    </p>
+                    <p className="bg-black cursor-pointer py-1 px-3 rounded-2xl text-[15px] hover:bg-white hover:text-black">
+                        Install App
+                    </p>
+                    <p className="flex items-center justify-center text-black bg-purple-500 rounded-full w-7 h-7">
+                        K
+                    </p>
+                </div>
+            </div>
+            <div className="flex items-center gap-2 mt-4">
+                <p className="px-4 py-1 text-black bg-white cursor-pointer rounded-2xl">
+                    All
+                </p>
+                <p className="px-4 py-1 bg-black cursor-pointer rounded-2xl">
+                    Music
+                </p>
+                <p className="px-4 py-1 bg-black cursor-pointer rounded-2xl">
+                    Podcasts
+                </p>
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
