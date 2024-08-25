5fddfb063c6a5c3cb1f041485974e75c8f432d70
Kanad Shee <codewizardkanad@gmail.com>
2024-08-25 21:54:37 +0530

    Added background oclor with more aesthetic way

--- Spotify/src/components/Display.jsx
+++ Spotify/src/components/Display.jsx
@@ -6,38 +6,41 @@ import { useContext } from "react";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Display = () => {
-  const { albumsData } = useContext(PlayerContext);
+    const { albumsData } = useContext(PlayerContext);
 
-  const displayRef = useRef();
-  const location = useLocation();
-  const isAlbum = location.pathname.includes("album");
-  const albumId = isAlbum ? location.pathname.split("/").pop() : "";
-  const album = isAlbum
-    ? albumsData.find((album) => album._id == albumId)
-    : null;
-  const bgColor = album ? album.bgColour : "#121212";
+    const displayRef = useRef();
+    const location = useLocation();
+    const isAlbum = location.pathname.includes("album");
+    const albumId = isAlbum ? location.pathname.split("/").pop() : "";
+    const album = isAlbum
+        ? albumsData.find((album) => album._id == albumId)
+        : null;
+    const bgColor = album ? album.bgColour : "#121212";
 
-  useEffect(() => {
-    if (isAlbum && album) {
-      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
-    } else {
-      displayRef.current.style.background = `#121212`;
-    }
-  }, [isAlbum, bgColor, album]);
+    useEffect(() => {
+        if (isAlbum && album) {
+            displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+        } else {
+            displayRef.current.style.background = `radial-gradient(circle, rgba(1,13,40,1) 10%, rgba(5,5,5,1) 100%)`;
+        }
+    }, [isAlbum, bgColor, album]);
 
-  return (
-    <div
-      ref={displayRef}
-      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
-    >
-      {albumsData.length > 0 ? (
-        <Routes>
-          <Route path="/" element={<DisplayHome />} />
-          <Route path="/album/:id" element={<DisplayAlbum album={album} />} />
-        </Routes>
-      ) : null}
-    </div>
-  );
+    return (
+        <div
+            ref={displayRef}
+            className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+        >
+            {albumsData.length > 0 ? (
+                <Routes>
+                    <Route path="/" element={<DisplayHome />} />
+                    <Route
+                        path="/album/:id"
+                        element={<DisplayAlbum album={album} />}
+                    />
+                </Routes>
+            ) : null}
+        </div>
+    );
 };
 
 export default Display;
